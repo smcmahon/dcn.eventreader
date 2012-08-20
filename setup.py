847d42227079ff7ac7431e20947e74f8a944455d
Steve McMahon <steve@dcn.org>
2012-08-20 09:49:13 -0700

    fix readme in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='dcn.eventreader',
       version=version,
       description="Read support for DCN Calendar",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
